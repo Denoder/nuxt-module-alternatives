aa9b747bc18c8fac98ff8bdee15ef940578c2af3
Teranode <Teranode@users.noreply.github.com>
2022-02-09 13:21:14 -0500

    Simplify code

--- @nuxtjs/vuex/vuex.ts
+++ @nuxtjs/vuex/vuex.ts
@@ -1,26 +1,7 @@
-import { createStore } from 'vuex'
+import store from '<%= options.store %>'
 import { defineNuxtPlugin } from '#app'
 
-let vuexStore = createStore({
-    strict: (process.env.NODE_ENV !== 'production'),
-    devtools: '<%= options.devtools %>',
-})
-
-const resolveStoreModules = (moduleData) => {
-    moduleData = moduleData.default || moduleData
-
-    Object.entries(moduleData).forEach(([name, storeData]) => {
-        vuexStore.registerModule(name, storeData)
-    });
-}
-
-export default defineNuxtPlugin(async context => {
-
-    "<% options.storeModules.forEach(s => { if (s.src.indexOf('index.') === 0) { %>"
-        resolveStoreModules(await import('<%= s.fullPath %>'))
-    "<% }}) %>"
-
-    context.vueApp.use(vuexStore)
-    context.provide('<%= options.storeName %>', vuexStore)
-    context['<%= options.storeName %>'] = vuexStore
+export default defineNuxtPlugin(async (context) => {
+    context.provide('<%= options.storeName %>', store)
+    context['<%= options.storeName %>'] = store
 })
