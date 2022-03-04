de41325c9a52ffb69593501982c34fd33fb564a3
Teranode <Teranode@users.noreply.github.com>
2022-03-04 08:46:50 -0500

    Delete @nuxtjs/auth directory

--- @nuxtjs/auth/inc/expired-auth-session-error.ts
+++
@@ -1,8 +0,0 @@
-export class ExpiredAuthSessionError extends Error {
-    constructor() {
-        super(
-            'Both token and refresh token have expired. Your request was aborted.'
-        )
-        this.name = 'ExpiredAuthSessionError'
-    }
-}
--- @nuxtjs/auth/schemes/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './base'
-export * from './cookie'
-export * from './local'
-export * from './oauth2'
-export * from './openIDConnect'
-export * from './refresh'
-
-export * from './auth0'
-export * from './laravel-jwt'
