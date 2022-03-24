812f93d366b33d9e7bb40203c495dad388a88267
Teranode <Teranode@users.noreply.github.com>
2022-03-24 06:20:21 -0400

    plugin fix

--- @nuxtjs-alt/auth/src/runtime/templates/auth.plugin.mjs
+++ @nuxtjs-alt/auth/src/runtime/templates/auth.plugin.mjs
@@ -5,40 +5,39 @@ import { defineNuxtPlugin, addRouteMiddleware } from '#app'
 <%= options.schemeImports.map(i => `import { ${i.name}${i.name !== i.as ? ' as ' + i.as : '' } } from '${i.from}'`).join('\n') %>
 
 export default defineNuxtPlugin(ctx => {
+
     // Options
     const options = JSON.parse('<%= JSON.stringify(options.options) %>')
 
-    ctx.hook('vue:setup', () => {
-        // Create a new Auth instance
-        const $auth = new Auth(ctx, options)
+    // Auth Middleware
+    addRouteMiddleware('auth', AuthMiddleWare, { global: options.globalMiddleware })
 
-        // Register strategies
-        <%=
-        options.strategies.map(strategy => {
-            const scheme = options.strategyScheme[strategy.name]
-            const schemeOptions = JSON.stringify(strategy, null, 2)
-            return `// ${strategy.name}\n  $auth.registerStrategy('${strategy.name}', new ${scheme.as}($auth, ${schemeOptions}))`
-        }).join('\n\n  ')
-        %>
+    // Create a new Auth instance
+    const $auth = new Auth(ctx, options)
 
-        // Auth Middleware
-        addRouteMiddleware('auth', AuthMiddleWare, { global: options.globalMiddleware })
+    // Register strategies
+    <%=
+    options.strategies.map(strategy => {
+        const scheme = options.strategyScheme[strategy.name]
+        const schemeOptions = JSON.stringify(strategy, null, 2)
+        return `// ${strategy.name}\n  $auth.registerStrategy('${strategy.name}', new ${scheme.as}($auth, ${schemeOptions}))`
+    }).join('\n\n  ')
+    %>
 
-        ctx.provide('auth', $auth);
+    ctx.provide('auth', $auth);
 
-        // Initialize auth
-        return $auth.init().catch(error => {
-            if (process.client) {
+    // Initialize auth
+    return $auth.init().catch(error => {
+        if (process.client) {
 
-            // Don't console log expired auth session errors. This error is common, and expected to happen.
-            // The error happens whenever the user does an ssr request (reload/initial navigation) with an expired refresh
-            // token. We don't want to log this as an error.
-            if (error instanceof ExpiredAuthSessionError) {
-                return
-            }
+        // Don't console log expired auth session errors. This error is common, and expected to happen.
+        // The error happens whenever the user does an ssr request (reload/initial navigation) with an expired refresh
+        // token. We don't want to log this as an error.
+        if (error instanceof ExpiredAuthSessionError) {
+            return
+        }
 
-            console.error('[ERROR] [AUTH]', error)
-            }
-        })
+        console.error('[ERROR] [AUTH]', error)
+        }
     })
 })
