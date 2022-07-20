58f11b2d20685a62264fe0ce42af488e86cb12c6
Teranode <Teranode@users.noreply.github.com>
2022-07-20 18:13:54 +0300

    [Http] Introduce Interceptors
    Since there was an issue with intercepting requests being overridden when using the native ohmyfetch request handlers, I decided to make an interceptor for this module that should be similar to axios. Not sure what bugs will pop up.

--- @nuxtjs-alt/http/src/runtime/templates/http.plugin.mjs
+++ @nuxtjs-alt/http/src/runtime/templates/http.plugin.mjs
@@ -12,10 +12,129 @@ class HttpInstance {
 
     constructor(defaults, $instance = http) {
         this.#httpDefaults = {
+            interceptors: [],
             ...defaults
         }
 
         this.#$fetch = $instance
+        this.#createMethods()
+    }
+
+    #createMethods() {
+        for (let method of ['get', 'head', 'delete', 'post', 'put', 'patch', 'options']) {
+            /**
+             * Method Creations
+             *
+             * @param {*} url
+             * @param {*} options
+             * @returns
+             */
+            Object.assign(this.__proto__, {
+                [method]: function (url, options) {
+                    const fetchOptions = defu(options, this.getDefaults())
+                    const interceptors = this.#httpDefaults.interceptors;
+
+                    if (fetchOptions && fetchOptions.params) {
+                        fetchOptions.params = cleanParams(options.params)
+                    }
+
+                    if (/^https?/.test(url)) {
+                        delete fetchOptions.baseURL
+                    }
+
+                    const controller = new AbortController();
+                    const timeoutSignal = setTimeout(() => controller.abort(), fetchOptions.timeout);
+                    delete fetchOptions.method
+
+                    let promise = Promise.resolve({ request: url, options: fetchOptions });
+
+                    interceptors.forEach(({ onRequest, onRequestError }) => {
+                        if (onRequest || onRequestError) {
+                            promise = promise.then(onRequest, onRequestError);
+                        }
+                    });
+
+                    promise = promise.then(context => {
+                        const request = context && context.request ? context.request : url
+                        const opt = context && options ? context.options : null
+
+                        return this.getFetch().raw(request, {
+                            method: method,
+                            signal: controller.signal,
+                            ...fetchOptions,
+                            ...opt
+                        })
+                        .then(response => {
+                            return response;
+                        })
+                        .catch(error => {
+                            return Promise.reject(error);
+                        });
+                    });
+
+                    interceptors.forEach(({ onResponse, onResponseError }) => {
+                        if (onResponse || onResponseError) {
+                            promise = promise.then(onResponse, onResponseError);
+                        }
+                    });
+
+                    clearTimeout(timeoutSignal);
+                    return promise
+                },
+                ['$' + method]: function (url, options) {
+                    const fetchOptions = defu(options, this.getDefaults())
+                    const interceptors = this.#httpDefaults.interceptors;
+
+                    if (fetchOptions && fetchOptions.params) {
+                        fetchOptions.params = cleanParams(options.params)
+                    }
+
+                    if (/^https?/.test(url)) {
+                        delete fetchOptions.baseURL
+                    }
+
+                    const controller = new AbortController();
+                    const timeoutSignal = setTimeout(() => controller.abort(), fetchOptions.timeout);
+                    delete fetchOptions.method
+
+                    let promise = Promise.resolve({ request: url, options: fetchOptions });
+
+                    interceptors.forEach(({ onRequest, onRequestError }) => {
+                        if (onRequest || onRequestError) {
+                            promise = promise.then(onRequest, onRequestError);
+                        }
+                    });
+
+                    promise = promise.then(context => {
+                        const request = context && context.request ? context.request : url
+                        const opt = context && options ? context.options : null
+
+                        let instance = this.getFetch().create({
+                            method: method,
+                            signal: controller.signal,
+                            ...fetchOptions,
+                            ...opt
+                        })
+
+                        return instance(request).then(response => {
+                            return response;
+                        })
+                        .catch(error => {
+                            return Promise.reject(error);
+                        });
+                    });
+
+                    interceptors.forEach(({ onResponse, onResponseError }) => {
+                        if (onResponse || onResponseError) {
+                            promise = promise.then(onResponse, onResponseError);
+                        }
+                    });
+
+                    clearTimeout(timeoutSignal);
+                    return promise
+                }
+            })
+        }
     }
 
     getFetch() {
@@ -49,8 +168,25 @@ class HttpInstance {
 
     create(options) {
         const { retry, timeout, baseURL, headers, credentials } = this.getDefaults()
-
         return createHttpInstance(defu(options, { retry, timeout, baseURL, headers, credentials }))
+    }
+
+    interceptors(interceptor) {
+        this.#httpDefaults.interceptors.push(interceptor);
+
+        () => { 
+            const index = this.#httpDefaults.interceptors.indexOf(interceptor);
+            if (index >= 0) {
+                this.#httpDefaults.interceptors.splice(index, 1);
+            }
+        }
+
+        return this
+    }
+
+    clearInterceptors () {
+        this.#httpDefaults.interceptors = [];
+        return this
     }
 }
 
@@ -58,95 +194,12 @@ const cleanParams = (obj) => {
     const cleanValues = [null, undefined, '']
     const cleanedObj = { ...obj };
     Object.keys(cleanedObj).forEach(key => {
-        if (cleanValues.includes(cleanedObj[key]) || (Array.isArray(cleanedObj[key]) && !cleanedObj[key].length) ) {
+        if (cleanValues.includes(cleanedObj[key]) || (Array.isArray(cleanedObj[key]) && !cleanedObj[key].length)) {
             delete cleanedObj[key];
         }
     });
 
     return cleanedObj;
-}
-
-for (let method of ['get', 'head', 'delete', 'post', 'put', 'patch', 'options']) {
-
-    /**
-     * Raw fetch instance
-     *
-     * @param {*} url
-     * @param {*} options
-     * @returns
-     */
-    HttpInstance.prototype[method] = async function (url, options) {
-        const fetchOptions = defu(options, this.getDefaults())
-        delete fetchOptions.method
-
-        if (fetchOptions && fetchOptions.params) {
-            fetchOptions.params = cleanParams(options.params)
-        }
-
-        if (/^https?/.test(url)) {
-            delete fetchOptions.baseURL
-        } else if (fetchOptions.baseURL && typeof url === 'string' && url.startsWith('/')) {
-            // Prevents `$fetch` from throwing "`input` must not begin with a slash when using `baseURL`"
-            url = url.slice(1)
-        }
-
-        try {
-            const controller = new AbortController();
-            const timeoutSignal = setTimeout(() => controller.abort(), fetchOptions.timeout);
-
-            let instance = await this.getFetch().raw(url, {
-                method: method,
-                signal: controller.signal,
-                ...fetchOptions
-            })
-
-            clearTimeout(timeoutSignal);
-            return instance
-        }
-        catch (error) {
-            console.error(error)
-        }
-    }
-
-    /**
-     * Destr/Parsed fetch instance
-     *
-     * @param {*} url
-     * @param {*} options
-     * @returns
-     */
-    HttpInstance.prototype['$' + method] = async function (url, options) {
-        const fetchOptions = defu(options, this.getDefaults())
-        delete fetchOptions.method
-
-        if (fetchOptions && fetchOptions.params) {
-            fetchOptions.params = cleanParams(options.params)
-        }
-
-        if (/^https?/.test(url)) {
-            delete fetchOptions.baseURL
-        } else if (fetchOptions.baseURL && typeof url === 'string' && url.startsWith('/')) {
-            // Prevents `$fetch` from throwing "`input` must not begin with a slash when using `baseURL`"
-            url = url.slice(1)
-        }
-
-        try {
-            const controller = new AbortController();
-            const timeoutSignal = setTimeout(() => controller.abort(), fetchOptions.timeout);
-
-            let instance = await this.getFetch().create({
-                method: method,
-                signal: controller.signal,
-                ...fetchOptions
-            })
-
-            clearTimeout(timeoutSignal);
-            return instance(url)
-        }
-        catch (error) {
-            console.error(error)
-        }
-    }
 }
 
 const createHttpInstance = (options) => {
@@ -174,15 +227,15 @@ export default defineNuxtPlugin(ctx => {
     }
 
     '<% if (options.proxyHeaders) { %>'
-        // Proxy SSR request headers
-        if (process.server && ctx.ssrContext.req && ctx.ssrContext.req.headers) {
-            const reqHeaders = { ...ctx.ssrContext.req.headers }
-            for (const h of '<%= options.proxyHeadersIgnore %>'.split(',')) {
-                delete reqHeaders[h]
-            }
-
-            defaults.headers = { ...reqHeaders, ...defaults.headers }
+    // Proxy SSR request headers
+    if (process.server && ctx.ssrContext.req && ctx.ssrContext.req.headers) {
+        const reqHeaders = { ...ctx.ssrContext.req.headers }
+        for (const h of '<%= options.proxyHeadersIgnore %>'.split(',')) {
+            delete reqHeaders[h]
         }
+
+        defaults.headers = { ...reqHeaders, ...defaults.headers }
+    }
     '<% } %>'
 
     if (process.server) {
